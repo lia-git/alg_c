25dbf0ddc047ad6e743e8e8461bbe983538a2e6c
Leo <leo@mac.localdomain>
2017-11-27 18:18:22 +0800

    update

---
+++ Solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Solution{
+
+public:
+    int minimumTotal(vector<vector<int> > &triangle) {
+
+           cout << "hello !!!" << endl;
+
+
+     }
+    };
+
+ int  main(){
+     cout << "hello !!!" << endl;
+
+ }
